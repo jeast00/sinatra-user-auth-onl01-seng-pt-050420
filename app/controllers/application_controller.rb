e03b9b4bb8979051f7b5bf017259e08a771b691f
Jonathan East <jonathan_east@ymail.com>
2020-08-01 00:00:52 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,9 +17,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
-    @user.save
-    session[:user_id] = @user.id
+    # @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    # @user.save
+    # session[:user_id] = @user.id
     puts params
     redirect '/users/home'
   end
